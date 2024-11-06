8ce03c80c52439b0cf72d8fa091f45df540f3e86
0ropo <he202225@students.ephec.be>
2024-11-06 12:05:05 +0100

    feat(frontEndCalendar): modification frontEnd pour afficher les calendriers récupérer de l'API google Calendar

--- server/models/calendar.go
+++ server/models/calendar.go
@@ -6,10 +6,13 @@ import (
 
 // Calendar représente le modèle d'un calendrier
 type Calendar struct {
-	ID        uint      `gorm:"primaryKey"`
-	UserID    uint      `gorm:"not null; uniqueIndex:idx_calendar_name; uniqueIndex:idx_calendar_url"`
-	Name      string    `gorm:"size:255; not null; uniqueIndex:idx_calendar_name"`
-	Url       string    `gorm:"size:255; not null; uniqueIndex:idx_calendar_url"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	IsActive  bool      `gorm:"default:true"`
+	ID     uint   `gorm:"primaryKey;autoIncrement"`
+	UserID uint   `gorm:"not null; uniqueIndex:idx_calendar_name; uniqueIndex:idx_calendar_idgoogle"`
+	Name   string `gorm:"size:255; not null; uniqueIndex:idx_calendar_name"`
+	//	Url       string    `gorm:"size:255; not null; uniqueIndex:idx_calendar_url"`
+	IDGoogle string `gorm:"size:255;not null;uniqueIndex:idx_calendar_idgoogle"`
+	//	Location  string `gorm:"size:255;not null;"`
+	Description string    `gorm:"size:255"`
+	CreatedAt   time.Time `gorm:"autoCreateTime"`
+	IsActive    bool      `gorm:"default:true"`
 }

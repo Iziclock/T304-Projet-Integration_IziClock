8971717198fd04c724f257eff9fc5e88f23d69b8
Hugo Troonbeeckx <h.troonbeeckx@students.ephec.be>
2024-11-07 18:21:49 +0100

    feat(ringtoneManagement): First test for the upload of ringtones on the VPS #40

--- server/routes/ringtones/ringtones.go
+++ server/routes/ringtones/ringtones.go
@@ -1,9 +1,7 @@
 package ringtones
 
 import (
-	"fmt"
 	"net/http"
-	"os/exec"
 	"path/filepath"
 
 	"github.com/gin-gonic/gin"
@@ -23,20 +21,20 @@ func upload_ringtone(context *gin.Context) {
 		return
 	}
 
-	savePath := filepath.Join("uploads", file.Filename)
+	savePath := filepath.Join("../../ringtones", file.Filename)
 	if err := context.SaveUploadedFile(file, savePath); err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to save the file"})
 		return
 	}
 
-	vpsPath := "iziclock@185.216.27.36:/home/iziclock/iziclock/ringtones"
-	scpCommand := fmt.Sprintf("scp -P 53152 %s %s", savePath, vpsPath)
+	// vpsPath := "iziclock@185.216.27.36:/home/iziclock/iziclock/ringtones"
+	// scpCommand := fmt.Sprintf("scp -P 53152 %s %s", savePath, vpsPath)
 
-	cmd := exec.Command("sh", "-c", scpCommand)
-	if err := cmd.Run(); err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error transferring file to VPS"})
-		return
-	}
+	// cmd := exec.Command("sh", "-c", scpCommand)
+	// if err := cmd.Run(); err != nil {
+	// 	context.JSON(http.StatusInternalServerError, gin.H{"error": "Error transferring file to VPS"})
+	// 	return
+	// }
 
 	context.JSON(http.StatusOK, gin.H{"message": "File uploaded and transferred successfully"})
 }

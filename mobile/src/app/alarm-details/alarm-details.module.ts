b7300a7f926944dcfaad3c2852729aaed9b59038
Hugo Troonbeeckx <h.troonbeeckx@students.ephec.be>
2024-12-18 03:38:16 +0100

    feat(prepTime): Calculation of the time the alarm is supposed to ring

--- mobile/src/app/alarm-details/alarm-details.module.ts
+++ mobile/src/app/alarm-details/alarm-details.module.ts
@@ -7,6 +7,7 @@ import { AlarmDetailsPageRoutingModule } from './alarm-details-routing.module';
 import { AlarmDetailsPage } from './alarm-details.page';
 import { AlarmInfoComponent } from '../components/alarm-info/alarm-info.component';
 import { TimePipe } from '../pipes/time.pipe';
+import { RingdatePipe } from '../pipes/ringdate.pipe';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { TimePipe } from '../pipes/time.pipe';
     FormsModule,
     IonicModule,
     AlarmDetailsPageRoutingModule,
-    TimePipe
+    TimePipe,
+    RingdatePipe
   ],
   declarations: [AlarmDetailsPage, AlarmInfoComponent]
 })
